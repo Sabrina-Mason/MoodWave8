7587f6febe8d311ac38aaaf49d76fb20f68e300d
Sabrina Mason <fireflyy85@gmail.com>
2017-08-17 17:01:44 -0400

    Ariana

---
+++ MoodWave8/Scripts/tommyshit.js
@@ -0,0 +1,22 @@
+﻿function setMood() {
+    var mood = $("#MoodBox option:selected").text();
+    //alert(mood)
+    var color;
+    if (mood === "Angry") {
+        color = "#FF0099";
+    } else if (mood === "Sad") {
+        color = "blue";
+    }
+    else if (mood === "Happy") {
+        color = "yellow";
+    } else if (mood === "Chill") {
+        color = "green";
+    }
+    else if (mood == "Neutral") {
+        color = "Gray"
+    }
+    $("#MoodBG").css("background-color", color);
+    $("#MoodBG").css("opacity", "0");
+    $("#MoodBG").animate
+        ({ opacity: 1 });
+}
